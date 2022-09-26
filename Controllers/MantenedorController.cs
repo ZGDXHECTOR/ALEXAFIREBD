37827c2d277864561092a5c15f99b31d51c1e870
Hector Martinez <elcuatloo44@gmail.com>
2022-09-26 11:15:45 -0500

    Agregar archivos de proyecto.

---
+++ Controllers/MantenedorController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using FireSharp;
+using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
+
+using Newtonsoft.Json;
+
+using ALEXAFIREBD.Models;
+
+namespace ALEXAFIREBD.Controllers
+{
+    public class MantenedorController : Controller
+    {
+
+
+        IFirebaseClient cliente;
+
+
+
+        public MantenedorController()
+        {
+            IFirebaseConfig config = new FirebaseConfig
+            {
+                AuthSecret = "Ig3vZgdkOzd19L1CMRgBJA7lUGqSvlum2VJmDs9e",
+                BasePath = "https://alexabd-1f52f-default-rtdb.firebaseio.com"
+
+
+
+            };
+
+
+
+            cliente = new FirebaseClient(config);
+        }
+         
+
+
+        // GET: Mantenedor
+        public ActionResult Inicio()
+        {
+            return View();
+        }
+        public ActionResult Crear()
+        {
+            return View();
+        }
+        public ActionResult Editar()
+        {
+            return View();
+        }
+        public ActionResult Eliminar()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Crear(Contacto oContacto)
+        {
+            string IdGenerado = Guid.NewGuid().ToString("N");
+
+            SetResponse response = cliente.Set("contactos/" + IdGenerado, oContacto);
+
+            if(response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                return View();
+            }
+            else
+            {
+                return View();
+            }
+
+
+            
+        }
+
+
+
+    }
+}
---
+++ Models/Contacto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ALEXAFIREBD.Models
+{
+    public class Contacto
+    {
+        public string IdContacto { get; set; }
+        public string Nombre { get; set; }
+        public string Correo { get; set; }
+        public string Contraseña { get; set; }
+
+    }
+}
